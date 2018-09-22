73bc8bf994e2882c73c20be215d38794e51ce4c7
Dylan Chong <dylan.chong@icloud.com>
2018-09-22 17:50:14 +1200

    Added firebase to app

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { createStackNavigator, type NavigationState } from 'react-navigation';
 import React, { Component } from 'react';
 
 type Props = { navigation: NavigationState };
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBO-WRCaI8AgCHfIE5WDmkpP3-uYW6zPUg',
+  authDomain: 'to-do-later.firebaseapp.com',
+  databaseURL: 'https://to-do-later.firebaseio.com',
+  projectId: 'to-do-later',
+  storageBucket: 'to-do-later.appspot.com',
+  messagingSenderId: '844883333608'
+};
+firebase.initializeApp(firebaseConfig);
 
 class WelcomeScreen extends Component<Props> {
   static navigationOptions = { title: 'Home' };

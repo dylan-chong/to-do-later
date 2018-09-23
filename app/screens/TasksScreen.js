0425cf02e0403908f6e9330a09995f5e987e26e8
Dylan Chong <dylan.chong@icloud.com>
2018-09-23 21:56:41 +1200

    Render list of tasks

--- app/screens/TasksScreen.js
+++ app/screens/TasksScreen.js
@@ -1,6 +1,18 @@
-import { Container, Fab, Icon } from 'native-base';
+import {
+  Body,
+  Container,
+  Fab,
+  Icon,
+  Left,
+  ListItem,
+  Right,
+  Text
+} from 'native-base';
+import { FlatList } from 'react-native';
 import { type NavigationState } from 'react-navigation';
 import React, { Component } from 'react';
+
+import { userData } from '../services/UserService';
 
 type Props = { navigation: NavigationState };
 
@@ -21,6 +33,22 @@ export class TasksScreen extends Component<Props> {
           onPress={ () => this.createTask() }>
           <Icon name="add" />
         </Fab>
+        <FlatList
+          data={ userData.currentUser().tasks }
+          renderItem={ entry =>
+              <ListItem itemDivider>
+                <Left />
+                <Body>
+                  <Text>
+                    { entry.item.title }
+                  </Text>
+                </Body>
+                <Right>
+                  <Icon active name="arrow-forward" />
+                </Right>
+              </ListItem>
+          }
+        />
       </Container>
     );
   }
